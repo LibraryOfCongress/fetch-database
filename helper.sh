18f747f77bec7f33c3ad7f77b083458c8678287c
Gregory Wolfe <gwolfe@loc.gov>
2024-03-22 19:49:37 -0500

    update container name to match elsewhere in app" "

--- helper.sh
+++ helper.sh
@@ -4,7 +4,7 @@
 # This may change to fit devops needs (e.g kubectl)
 # This will also not build on local docker network
 build-inventory-db () {
-    docker run -d --name inventory-postgres -p 5432:5432 \
+    docker run -d --name inventory-database -p 5432:5432 \
     -v inventory_service_data:/var/lib/postgresql/data \
     inventory-postgres-image
 }
@@ -13,7 +13,7 @@ build-inventory-db () {
 # This may change to fit devops needs (e.g. kubectl)
 # This will also not build on local docker network
 wipe-inventory-db () {
-    docker run -d --name inventory-postgres -p 5432:5432 \
+    docker run -d --name inventory-database -p 5432:5432 \
     -v inventory_service_data:/var/lib/postgresql/data \
     -e WIPE_VOLUME=true \
     inventory-postgres-image
